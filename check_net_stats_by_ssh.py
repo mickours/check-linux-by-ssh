03cefa1aab5414f10c8ec7331d1e5ab46e5e88af
Michael Mercier <mickours@gmail.com>
2014-07-22 17:19:05 +0200

    Makes it work with busybox (ls option not found)
    Use stat instead of ls/awk to avoid this busybox error:
    ```
    ls: unrecognized option '--time-style=+%s'                                                                                                           [169/811]
    BusyBox v1.19.0 (2012-11-21 18:06:14 PST) multi-call binary.
    ```

--- check_net_stats_by_ssh.py
+++ check_net_stats_by_ssh.py
@@ -80,7 +80,7 @@ def get_net_stats(client):
    
     # Beware of the export!
     raw = 'CHK_FILE=%s;' % DEFAULT_TEMP_FILE
-    raw += r"""date +%s;ls -l --time-style=+%s $CHK_FILE | awk '{print $6}';cat $CHK_FILE; sed 1,2d /proc/net/dev | tee $CHK_FILE"""
+    raw += r"""date +%s; stat -c %Y $CHK_FILE; cat $CHK_FILE; sed 1,2d /proc/net/dev | tee $CHK_FILE"""
     stdin, stdout, stderr = client.exec_command('export LC_LANG=C && unset LANG && %s' % raw)
 
     errors = [l for l in stderr]
